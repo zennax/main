dfd5ea22cdd0e018715d099062ee0575d87bbe50
slyk0 <henrykelp@proton.me>
2023-11-14 21:59:58 -0500

    better audio player

--- temp/temp.js
+++ temp/temp.js
@@ -4,17 +4,32 @@ const progress = document.getElementById('progress');
 const volume = document.getElementById('volume');
 const playlist = document.getElementById('playlist');
 const playlistItems = playlist.getElementsByTagName('li');
+const currentSongTitleDisplay = document.getElementById('currentSongTitle'); // Updated line
 
 playPauseBtn.addEventListener('click', togglePlayPause);
 audio.addEventListener('timeupdate', updateProgress);
 progress.addEventListener('input', setProgress);
 volume.addEventListener('input', setVolume);
 
+// Variable to store the currently selected playlist item
+let currentPlaylistItem = null;
+
 for (let i = 0; i < playlistItems.length; i++) {
     playlistItems[i].addEventListener('click', function () {
         const songSrc = this.getAttribute('data-src');
+
+        // Remove the highlight class from the previously selected item
+        if (currentPlaylistItem) {
+            currentPlaylistItem.classList.remove('highlight');
+        }
+
+        // Add the highlight class to the clicked item
+        this.classList.add('highlight');
+        currentPlaylistItem = this;
+
         changeSong(songSrc);
         togglePlayPause();
+        updateCurrentSongTitleDisplay();
     });
 }
 
@@ -49,4 +64,20 @@ function setVolume() {
 function changeSong(songSrc) {
     audio.src = songSrc;
     audio.load();
+}
+
+// Function to update the currently playing song title display
+function updateCurrentSongTitleDisplay() {
+    const currentSongTitle = getCurrentSongTitle();
+    currentSongTitleDisplay.innerHTML = currentSongTitle;
+}
+
+// Function to get the title of the currently playing song
+function getCurrentSongTitle() {
+    for (let i = 0; i < playlistItems.length; i++) {
+        if (playlistItems[i].getAttribute('data-src') === audio.src) {
+            return playlistItems[i].innerHTML;
+        }
+    }
+    return ''; // Return an empty string if the song title is not found
 }
