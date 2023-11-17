5c43615c68a8b8b9de7da3557975c7b09d48323e
slyk0 <henrykelp@proton.me>
2023-11-17 14:22:04 -0500

    Updated search placeholder
    updated xxx

--- scripts/alert.js
+++ scripts/alert.js
@@ -1,1 +1,5 @@
-alert("Website isn't finished.");
+const userConsent = confirm("This website is not finished. Proceed?");
+    
+if (!userConsent) {
+    alert("Thank you for your understanding. Feel free to come back later when the website is complete.");
+}
